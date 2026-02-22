7c63d21926ae1b2cb0f752c125ff9da153fbb301
24f2006813 <24f2006813@ds.study.iitm.ac.in>
2026-02-22 17:21:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,21 @@ EMAIL = "24f2006813@ds.study.iitm.ac.in"
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    # The grader looks for this exact string in the body
-    return f"<html><body>{EMAIL}</body></html>"
+    # We add or use a span to break the pattern 
+    # Cloudflare's scraper won't see it as a single email string
+    part1, part2 = EMAIL.split("@")
+    tricked_email = f'<span>{part1}</span>@<span>{part2}</span>'
+    
+    # We also add the data-cfemail="false" attribute to hint to Cloudflare to stop
+    return f"""
+    <html>
+        <body>
+            <div data-cfemail="false">
+                {tricked_email}
+            </div>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
-    # host="0.0.0.0" is REQUIRED for localtunnel to connect
     uvicorn.run(app, host="0.0.0.0", port=5501)
